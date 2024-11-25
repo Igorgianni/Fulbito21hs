e58b6b59ec6aeeabbad23b67e5c794a002304601
Igorgianni <126618797+Igorgianni@users.noreply.github.com>
2024-11-25 18:30:00 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Fulbito21hs',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
