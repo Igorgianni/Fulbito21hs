00c3495e703aea6a967bad81bbfa12ef385dd7d2
Igorgianni <126618797+Igorgianni@users.noreply.github.com>
2024-11-25 18:23:02 -0300

    Create types.ts

---
+++ app/types.ts
@@ -0,0 +1,13 @@
+export interface Player {
+  id: number
+  name: string
+  physicalCondition: number
+  shooting: number
+  passing: number
+  goalkeeping: number
+  defense: number
+  dribbling: number
+  speed: number
+  competitiveness: number
+}
+
