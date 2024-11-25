8472f063631f54aa28231de15dd0a91b89cf3f20
Igorgianni <126618797+Igorgianni@users.noreply.github.com>
2024-11-25 18:20:55 -0300

    Create players.ts

---
+++ app/data/players.ts
@@ -0,0 +1,124 @@
+import { Player } from '../types'
+
+export const players: Player[] = [
+  {
+    id: 1,
+    name: "Mati L",
+    physicalCondition: 9,
+    shooting: 10,
+    passing: 10,
+    goalkeeping: 3,
+    defense: 5,
+    dribbling: 10,
+    speed: 9,
+    competitiveness: 10
+  },
+  {
+    id: 2,
+    name: "Tama",
+    physicalCondition: 10,
+    shooting: 10,
+    passing: 8,
+    goalkeeping: 3,
+    defense: 4,
+    dribbling: 9,
+    speed: 9,
+    competitiveness: 10
+  },
+  {
+    id: 3,
+    name: "Rama",
+    physicalCondition: 8,
+    shooting: 9,
+    passing: 9,
+    goalkeeping: 2,
+    defense: 4,
+    dribbling: 10,
+    speed: 9,
+    competitiveness: 9
+  },
+  {
+    id: 4,
+    name: "Lucho Z",
+    physicalCondition: 9,
+    shooting: 9,
+    passing: 8,
+    goalkeeping: 2,
+    defense: 5,
+    dribbling: 9,
+    speed: 10,
+    competitiveness: 9
+  },
+  {
+    id: 5,
+    name: "Igor",
+    physicalCondition: 9,
+    shooting: 8,
+    passing: 10,
+    goalkeeping: 3,
+    defense: 6,
+    dribbling: 8,
+    speed: 8,
+    competitiveness: 9
+  },
+{
+      id: 6,
+    name: "Fercho",
+    physicalCondition: 9,
+    shooting: 10,
+    passing: 10,
+    goalkeeping: 3,
+    defense: 5,
+    dribbling: 10,
+    speed: 9,
+    competitiveness: 10
+  },
+  {
+    id: 7,
+    name: "Gime",
+    physicalCondition: 10,
+    shooting: 10,
+    passing: 8,
+    goalkeeping: 3,
+    defense: 4,
+    dribbling: 9,
+    speed: 9,
+    competitiveness: 10
+  },
+  {
+    id: 8,
+    name: "ElPelotudodelColo",
+    physicalCondition: 8,
+    shooting: 9,
+    passing: 9,
+    goalkeeping: 2,
+    defense: 4,
+    dribbling: 10,
+    speed: 9,
+    competitiveness: 9
+  },
+  {
+    id: 9,
+    name: "Vecino",
+    physicalCondition: 9,
+    shooting: 9,
+    passing: 8,
+    goalkeeping: 2,
+    defense: 5,
+    dribbling: 9,
+    speed: 10,
+    competitiveness: 9
+  },
+  {
+    id: 10,
+    name: "Jani",
+    physicalCondition: 9,
+    shooting: 8,
+    passing: 10,
+    goalkeeping: 3,
+    defense: 6,
+    dribbling: 8,
+    speed: 8,
+    competitiveness: 9
+  // Agrega más jugadores...
+]
